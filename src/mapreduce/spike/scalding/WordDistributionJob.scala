e663ed8c5243c54b5c3fde056e8813a435aad994
Sriram <sriram@indix.com>
2014-07-21 15:00:26 +0530

    Added reverseSortedTake for top K in Scalding example

--- src/mapreduce/spike/scalding/WordDistributionJob.scala
+++ src/mapreduce/spike/scalding/WordDistributionJob.scala
@@ -5,17 +5,18 @@ import com.twitter.scalding._
 class WordDistributionJob(args: Args) extends Job(args) {
   val input = args("input")
   val output = args("output")
+  val K = 3
 
   def tokenize(line: String) = line.split(" ").map(_.trim).filterNot(_.isEmpty)
-  def shortWord(word: String) = word.length <= 2
+  def stopWord(word: String) = word.length <= 2
 
   TextLine(input)
   .name("WordDistribution Scalding")
   .flatMapTo('line -> 'word)(tokenize)
-  .filter(! shortWord)
+  .filter('line)(!stopWord)
   .map('word -> 'count)(1) // Creates a count field for every record(containing only the word so far).
   .groupBy('word)(_.sum('count, 'frequency)) // this is beautiful, it does a group by along with doing a map-side aggregation.
-  .groupAll(_.sortBy('frequency).reverse)
+  .groupAll(_.sortedReverseTake(('frequency, 'word), K))
   .project('word, 'frequency)
   .write(Tsv(output, ('word, 'frequency)))
 
